d179b5b792643c41fcd18fbf8838ba872762ec4f
manishraj <wingsmanish11@gmail.com>
2025-08-27 00:23:28 +0530

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 "use client"
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem, } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { Redirect } from "next";
+
 
 
 const Navbar = ({ className }: { className?: string }) => {
--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -1,8 +1,6 @@
 "use client";
-
 import React, { useState, useRef } from "react";
 import Image from "next/image";
-import { MouseEvent } from "react";
 import {
   motion,
   useTransform,
@@ -48,7 +46,7 @@ export const AnimatedTooltip = ({
 
   return (
     <>
-      {items.map((item, idx) => (
+      {items.map((item) => (
         <div
           className="group relative -mr-4"
           key={item.name}
